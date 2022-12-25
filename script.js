3712addbcae97ed3342da83c01f41bdacc99639a
Chamudi R Wijesekera <chamudirw1802@gmail.com>
2022-12-25 14:42:47 +0530

    code format

--- script.js
+++ script.js
@@ -1,71 +1,71 @@
+const videoElement = document.getElementById('videoElement');
+const btnTurnOn = document.getElementById('btnTurnOn');
+const btnTurnOff = document.getElementById('btnTurnOff');
+const btnRecord = document.getElementById('btnRecord');
+const btnStop = document.getElementById('btnStop');
+const downloadLink = document.getElementById('downloadLink');
+const constraints = {
+  audio: false,
+  video: { width: 640, height: 480 },
+};
+let stream;
+let mediaRecorder;
+let recordedBlobs;
 
-    const videoElement = document.getElementById('videoElement');
-    const btnTurnOn = document.getElementById('btnTurnOn');
-    const btnTurnOff = document.getElementById('btnTurnOff');
-    const btnRecord = document.getElementById('btnRecord');
-    const btnStop = document.getElementById('btnStop');
-    const downloadLink = document.getElementById('downloadLink');
-    const constraints = {
-      audio: false,
-      video: { width: 640, height: 480 }
-    };
-    let stream;
-    let mediaRecorder;
-    let recordedBlobs;
+btnTurnOn.addEventListener('click', () => {
+  navigator.mediaDevices.getUserMedia(constraints).then((s) => {
+    stream = s;
+    videoElement.srcObject = stream;
+  });
+});
 
-    btnTurnOn.addEventListener('click', () => {
-      navigator.mediaDevices.getUserMedia(constraints)
-        .then((s) => {
-          stream = s;
-          videoElement.srcObject = stream;
-        });
-    });
+btnTurnOff.addEventListener('click', () => {
+  stream.getTracks().forEach((track) => {
+    track.stop();
+  });
+  videoElement.srcObject = null;
+});
 
-    btnTurnOff.addEventListener('click', () => {
-      stream.getTracks().forEach((track) => {
-        track.stop();
-      });
-      videoElement.srcObject = null;
-    });
-
-    btnRecord.addEventListener('click', () => {
-      recordedBlobs = [];
-      let options = { mimeType: 'video/webm;codecs=vp9' };
+btnRecord.addEventListener('click', () => {
+  recordedBlobs = [];
+  let options = { mimeType: 'video/webm;codecs=vp9' };
+  if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+    console.error(`${options.mimeType} is not supported`);
+    options = { mimeType: 'video/webm;codecs=vp8' };
+    if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+      console.error(`${options.mimeType} is not supported`);
+      options = { mimeType: 'video/webm' };
       if (!MediaRecorder.isTypeSupported(options.mimeType)) {
         console.error(`${options.mimeType} is not supported`);
-        options = { mimeType: 'video/webm;codecs=vp8' };
-        if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-          console.error(`${options.mimeType} is not supported`);
-          options = { mimeType: 'video/webm' };
-          if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-            console.error(`${options.mimeType} is not supported`);
-            options = { mimeType: '' };
-          }
-        }
+        options = { mimeType: '' };
       }
+    }
+  }
 
-      try {
-        mediaRecorder = new MediaRecorder(stream, options);
-      } catch (e) {
-        console.error('Exception while creating MediaRecorder:', e);
-        return;
-      }
+  try {
+    mediaRecorder = new MediaRecorder(stream, options);
+  } catch (e) {
+    console.error('Exception while creating MediaRecorder:', e);
+    return;
+  }
 
-      mediaRecorder.ondataavailable = (event) => {
-        if (event.data && event.data.size > 0) {
-          recordedBlobs.push(event.data);
-}
-};
+  mediaRecorder.ondataavailable = (event) => {
+    if (event.data && event.data.size > 0) {
+      recordedBlobs.push(event.data);
+    }
+  };
   mediaRecorder.start(10);
 });
 
 btnStop.addEventListener('click', () => {
   mediaRecorder.stop();
-  downloadLink.href = URL.createObjectURL(new Blob(recordedBlobs, { type: 'video/webm' }));
+  downloadLink.href = URL.createObjectURL(
+    new Blob(recordedBlobs, { type: 'video/webm' })
+  );
   downloadLink.download = 'recorded-video.webm';
   downloadLink.style.display = 'block';
 });
- const recordingIndicator = document.getElementById('recordingIndicator');
+const recordingIndicator = document.getElementById('recordingIndicator');
 
 btnRecord.addEventListener('click', () => {
   // ...
@@ -76,4 +76,3 @@ btnStop.addEventListener('click', () => {
   // ...
   recordingIndicator.innerHTML = '';
 });
-
